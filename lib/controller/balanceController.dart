90d11cd8b511100b680cca659274ae9d55819553
chanakan <chonakankongyen@gmail.com>
2025-08-23 21:59:50 +0700

    fix: Add newline for better code readability in BalanceController

--- lib/controller/balanceController.dart
+++ lib/controller/balanceController.dart
@@ -17,6 +17,7 @@ class BalanceController {
     throw Exception('Failed to top up balance');
   }
 
+
   Future<double> withdrawBalance(int memberId, double amount) async {
     final url = Uri.parse('$baseURL/members/$memberId/withdraw');
     final response = await http.post(
